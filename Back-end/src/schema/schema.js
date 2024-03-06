16af678ef204d1537609589090999210fdbebd71
TrImEaS <trimeas@hotmail.com>
2024-03-06 14:06:24 -0300

    add

--- Back-end/src/schema/schema.js
+++
@@ -1,7 +0,0 @@
-import { blob, sqliteTable } from "drizzle-orm/sqlite-core";
-
-export const productImages = sqliteTable('product_images', {
-  id: blob('id').primaryKey(),
-  name: blob('name').notNull(),
-  image: blob('image_data').notNull(),
-});
